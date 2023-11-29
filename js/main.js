48400aece4e39e9731fce9cd3250568fd6a7a189
RiosGaston <gastonrios2016@gmail.com>
2023-11-29 18:01:58 -0300

    se agrego lo requerido en la PreEntrega numero 3

--- js/main.js
+++ js/main.js
@@ -1,119 +1,124 @@
-function Calculadora() {
-    this.elegirOperacion = "";
-    this.memoria = [];
+document.addEventListener("DOMContentLoaded", function() {
+    function Calculadora() {
+        this.memoria = JSON.parse(localStorage.getItem('calculadora_memoria')) || [];
 
-    this.obtenerOperacion = function() {
-        do {
-            this.elegirOperacion = prompt("Ingrese alguna de las siguientes opciones (+, -, *, o /) para realizar una operación matemática:");
-        } while (this.elegirOperacion !== "+" && this.elegirOperacion !== "-" && this.elegirOperacion !== "*" && this.elegirOperacion !== "/");
-    }
-
-    this.numeros = function() {
-        this.num1 = parseFloat(prompt("Ingrese el primer número:"));
-        this.num2 = parseFloat(prompt("Ingrese el segundo número:"));        
-    }
-
-    this.resultados = function() {
-        let resultado;
-
-        switch(this.elegirOperacion) {
-            case "+":
-                resultado = this.num1 + this.num2;
-                alert("El resultado de la suma es: " + resultado);
-                break;
-                
-            case "-":
-                resultado = this.num1 - this.num2;
-                alert("El resultado de la resta es: " + resultado);
-                break;
-
-            case "*":
-                resultado = this.num1 * this.num2;
-                alert("El resultado de la multiplicación es: " + resultado);
-                break;
-
-            case "/":
-                resultado = this.num1 / this.num2;
-                alert("El resultado de la división es: " + resultado);
-                break;
+        this.obtenerNumeros = function() {
+            this.num1 = parseFloat(document.getElementById("num1").value);
+            this.num2 = parseFloat(document.getElementById("num2").value);
         }
 
-        this.memoria.push(resultado);
-        console.log(this.memoria);
-    }
+        this.resultados = function(resultado, operacion) {
+            let resultadoElement = document.getElementById("resultado");
+            resultadoElement.textContent = resultado !== undefined ? `Resultado de ${this.num1} ${operacion} ${this.num2}: ${resultado}` : "Error: Debe ingresar un número en ambos campos";
+        }
 
-    this.mostrarResultados = function() {
-        let elegirResultado = prompt("¿Qué resultado quiere ver? Escriba un numero o la palabra (todos) para que se muestren todos los resultados. Ej: 1 (muestra el primer resultado)");
+        this.mostrarMemoria = function() {
+            let memoriaList = document.getElementById("memoriaList");
+            memoriaList.innerHTML = "";
 
-        if (elegirResultado.toLowerCase() === "todos") {
             this.memoria.forEach(function(resultado, index) {
-                alert(`Resultado ${index + 1}: ${resultado}`);
+                let li = document.createElement("li");
+                li.textContent = `Resultado ${index + 1}: ${resultado}`;
+                memoriaList.appendChild(li);
             });
-        } else {
-            let index = parseInt(elegirResultado);
-            if (!isNaN(index) && index >= 1 && index <= this.memoria.length) {
-                alert(`El resultado de la cuenta seleccionada es: ${this.memoria[index - 1]}`);
-            } else {
-                alert("Opción no válida");
+        }
+
+        this.limpiarCampos = function() {
+            document.getElementById("num1").value = "";
+            document.getElementById("num2").value = "";
+        }
+
+        this.limpiarMemoria = function() {
+            this.memoria = [];
+            this.mostrarMemoria();
+            this.guardarMemoriaEnLocalStorage();
+        }
+
+        this.validarCampos = function() {
+            return !isNaN(this.num1) && !isNaN(this.num2);
+        }
+
+        this.realizarOperacion = function(operacion) {
+            this.obtenerNumeros();
+
+            if (!this.validarCampos()) {
+                this.resultados();
+                return;
             }
+
+            let resultado;
+            switch(operacion) {
+                case "+":
+                    resultado = this.num1 + this.num2;
+                    break;
+                case "-":
+                    resultado = this.num1 - this.num2;
+                    break;
+                case "*":
+                    resultado = this.num1 * this.num2;
+                    break;
+                case "/":
+                    resultado = this.num1 / this.num2;
+                    break;
+            }
+
+            this.memoria.push(`${this.num1} ${operacion} ${this.num2} = ${resultado}`);
+            this.resultados(resultado, operacion);
+            this.mostrarMemoria();
+            this.limpiarCampos();
+            this.guardarMemoriaEnLocalStorage();
+        }
+
+        this.guardarMemoriaEnLocalStorage = function() {
+            localStorage.setItem('calculadora_memoria', JSON.stringify(this.memoria));
+        }
+
+        this.ejecutarCalculadora = function(operacion) {
+            this.realizarOperacion(operacion);
         }
     }
 
-    this.filtrar = function() {
-        let tipoFiltro = prompt("¿Qué tipo de filtro quiere utilizar? Escriba 'menor' o 'mayor':");
-        let filtro = parseFloat(prompt(`Ingrese el valor para el filtro ${tipoFiltro}:`));
+    const miCalculadora = new Calculadora();
 
-        let resultadosFiltrados;
+    document.getElementById("btnSumar").addEventListener("click", function() {
+        miCalculadora.ejecutarCalculadora("+");
+    });
 
-        if (tipoFiltro === "menor") {
-            resultadosFiltrados = this.memoria.filter(function(resultado) {
-                return resultado < filtro;
-            });
-        } else if (tipoFiltro === "mayor") {
-            resultadosFiltrados = this.memoria.filter(function(resultado) {
-                return resultado > filtro;
-            });
-        } else {
-            alert("Tipo de filtro no válido. Por favor, ingrese 'menor' o 'mayor'.");
-            return;
+    document.getElementById("btnRestar").addEventListener("click", function() {
+        miCalculadora.ejecutarCalculadora("-");
+    });
+
+    document.getElementById("btnMultiplicar").addEventListener("click", function() {
+        miCalculadora.ejecutarCalculadora("*");
+    });
+
+    document.getElementById("btnDividir").addEventListener("click", function() {
+        miCalculadora.ejecutarCalculadora("/");
+    });
+
+    document.getElementById("btnLimpiarMemoria").addEventListener("click", function() {
+        miCalculadora.limpiarMemoria();
+    });
+
+    document.addEventListener("keydown", function(event) {
+        switch(event.key) {
+            case "+":
+                miCalculadora.ejecutarCalculadora("+");
+                break;
+            case "-":
+                miCalculadora.ejecutarCalculadora("-");
+                break;
+            case "*":
+                miCalculadora.ejecutarCalculadora("*");
+                break;
+            case "/":
+                miCalculadora.ejecutarCalculadora("/");
+                break;
+            case "Backspace":
+                miCalculadora.limpiarMemoria();
+                break;
         }
+    });
 
-        if (resultadosFiltrados.length > 0) {
-            alert(`Resultados ${tipoFiltro}es a ${filtro}: ${resultadosFiltrados}`);
-        } else {
-            alert(`No hay resultados ${tipoFiltro}es a ${filtro}`);
-        }
-    }
-
-    this.otrasOperaciones = function() {
-        let otraOperacion = prompt("1) Desea realizar otra operación matemática? Escriba si o no.\n2) Si desea ver los resultados de las operaciones realizadas escriba resultados.\n3) Si desea filtrar resultados escriba la palabra filtrar");
-        let textoMinusculas = otraOperacion.toLowerCase();
-    
-        if (textoMinusculas == "si") {
-            this.ejecutarCalculadora();
-        } else if (textoMinusculas == "no") {
-            alert("Gracias por usar la calculadora");
-        } else if (textoMinusculas == "resultados") {
-            this.mostrarResultados();
-            this.otrasOperaciones();
-        } else if (textoMinusculas == "filtrar") {
-            this.filtrar();
-            this.otrasOperaciones();
-        } else {
-            do {
-                otraOperacion = prompt("1) Desea realizar otra operación matemática? Escriba si o no.\n2) Si desea ver los resultados de las operaciones realizadas escriba resultados.\n3) Si desea filtrar resultados escriba la palabra filtrar");
-            } while (otraOperacion !== "si" && otraOperacion !== "no");
-            this.otrasOperaciones();
-        }
-    }
-    
-    this.ejecutarCalculadora = function() {
-        this.obtenerOperacion();
-        this.numeros();
-        this.resultados();
-        this.otrasOperaciones();
-    }
-}
-
-const miCalculadora = new Calculadora();
-miCalculadora.ejecutarCalculadora();
+    miCalculadora.mostrarMemoria();
+});
